931ef4d2c8e5b53cd4f5e349fc9870f5d3cdca47
sandormatyas <sandormatyas7@gmail.com>
2020-05-03 20:47:39 +0200

    configure sshd

--- provision.sh
+++ provision.sh
@@ -2,3 +2,6 @@
 sudo adduser admin --gecos "" --disabled-password
 echo "admin:admin" | sudo chpasswd
 ssh-copy-id - i ./id_rsa.pub admin@localhost
+awk '/PermitRootLogin/ {gsub(/yes/, "no")};{print}' /etc/ssh/sshd_config > /etc/ssh/sshd_config
+awk '/PasswordAuthentication/ {gsub(/yes/, "no")};{print}' /etc/ssh/sshd_config > /etc/ssh/sshd_config
+service sshd restart
