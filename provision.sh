38aeeb4f7829aed5cef9dfcd7532f3f73277b18f
sandormatyas <sandormatyas7@gmail.com>
2020-05-04 00:38:02 +0200

    add password prompt to provision

--- provision.sh
+++ provision.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+sudo apt install sshpass
 sudo adduser admin --gecos "" --disabled-password
 echo "admin:admin" | sudo chpasswd
-ssh-copy-id - i ./id_rsa.pub admin@localhost
+echo "admin" | shhpass ssh-copy-id -f -i ./id_rsa.pub admin@localhost
 awk '/PermitRootLogin/ {gsub(/yes/, "no")};{print}' /etc/ssh/sshd_config > /etc/ssh/sshd_config
 awk '/PasswordAuthentication/ {gsub(/yes/, "no")};{print}' /etc/ssh/sshd_config > /etc/ssh/sshd_config
 service sshd restart
