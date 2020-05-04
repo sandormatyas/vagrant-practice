0ac9b7739e9e35880e4bfdd3bd4bc32cf30edf53
sandormatyas <sandormatyas7@gmail.com>
2020-05-04 12:58:23 +0200

    change to provision file

--- Vagrantfile
+++ Vagrantfile
@@ -1,4 +1,5 @@
 Vagrant.configure("2") do |config|
 	config.vm.box = "ubuntu/bionic64"
-	config.vm.provision "file", source: "./", destination: "$HOME/provisions"
+	config.vm.provision "file", source: "./id_rsa.pub", destination:"./"
+	config.vm.provision "shell", path: "provision.sh"
 end
--- provision.sh
+++ provision.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-sudo apt install sshpass
-sudo adduser admin --gecos "" --disabled-password
+sudo apt update && sudo apt install sshpass
+sudo adduser admin --gecos "" --ingroup admin --disabled-password
 echo "admin:admin" | sudo chpasswd
-echo "admin" | shhpass ssh-copy-id -f -i ./id_rsa.pub admin@localhost
-awk '/PermitRootLogin/ {gsub(/yes/, "no")};{print}' /etc/ssh/sshd_config > /etc/ssh/sshd_config
-awk '/PasswordAuthentication/ {gsub(/yes/, "no")};{print}' /etc/ssh/sshd_config > /etc/ssh/sshd_config
-service sshd restart
+echo "admin" | sshpass ssh-copy-id -f -i ./id_rsa.pub admin@localhost
+sudo awk '/PermitRootLogin/ {gsub(/yes/, "no");gsub(/#/, "")} /PasswordAuthentication/ {gsub(/yes/, "no");gsub(/#/, "")};{print}' /etc/ssh/sshd_config > /etc/ssh/sshd_config
+sudo service ssh restart
